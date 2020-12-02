75850f2f6c53424a7045d771da00812a5164adcc
KaleevaAlina <69644331+KaleevaAlina@users.noreply.github.com>
2020-12-02 16:57:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+"use strict"
+
+//Задача 1
+function onclick1() {
+	let butt = document.getElementById('but0');
+	alert('Нажали на меня!!!')
+}
+
+//Задача 2
+function butmouseover() {
+	let but = document.getElementById('but4');
+	alert('Привет!');
+}
+
+//Задача 3
+function onDblclick() {
+	let input = document.getElementById('but');
+	alert('Привет!!!!!!!!!')
+}
+
+//Задача 4
+function but1mouseover() {
+}	
+function but1mouseout() {
+	alert('Привет!!!!!!!!!!!!');
+}	
+
+//Задача 5
+function button1Click() {
+	// let input2 = document.getElementById('button1');
+	/*  alert('Ку-ку');  */
+	// var number1 = input2.value;
+	// console.log(number1);
+	 alert('Kу-ку');
+}
